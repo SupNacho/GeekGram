21ce6066b33ae7416c7385f039f8d8fe0e773eb2
SuperNacho <vanbellum@gmail.com>
2018-03-06 17:02:36 +0300

    added main fragment initialization and navigation drawer

--- app/src/main/java/geekgram/supernacho/ru/MainActivity.java
+++ app/src/main/java/geekgram/supernacho/ru/MainActivity.java
@@ -1,8 +1,13 @@
 package geekgram.supernacho.ru;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,14 +15,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MainFragment.OnFragmentInteractionListener {
+    private MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initNavDrawer();
+        initFragment();
+    }
+
+    private void initFragment() {
+        mainFragment = MainFragment.newInstance();
+        FragmentTransaction fragmentManager = getSupportFragmentManager().beginTransaction();
+        fragmentManager.add(R.id.main_fragment_container, mainFragment);
+        fragmentManager.commit();
+    }
+
+    private void initNavDrawer() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,6 +48,23 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //ДЗ пункт №3
+        int[][] states = new int[][]{
+                new int[]{-android.R.attr.state_enabled},
+                new int[]{-android.R.attr.state_checked},
+                new int[]{android.R.attr.state_checked},
+                new int[]{android.R.attr.state_pressed},
+                new int[]{android.R.attr.state_enabled}};
+        int[] colors = new int[]{
+                Color.MAGENTA,
+                Color.BLACK,
+                Color.RED,
+                Color.GREEN,
+                Color.BLUE};
+
+        navigationView.setItemTextColor(new ColorStateList(states, colors));
+        navigationView.setItemIconTintList(new ColorStateList(states, colors));
     }
 
     @Override
@@ -50,12 +86,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -68,24 +100,19 @@ public class MainActivity extends AppCompatActivity
         // TODO: 06.03.2018 implement fragment interaction
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_main:
+                Toast.makeText(this, "Main clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_theme:
+                Intent themeIntent = new Intent(MainActivity.this, ThemeActivity.class);
+                startActivity(themeIntent);
+                break;
+            default:
+                Toast.makeText(this, "Menu Item not found", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
