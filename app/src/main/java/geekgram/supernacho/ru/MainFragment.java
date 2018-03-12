e114b5853c7cdd8c4501252732a1d7d3c16fb5c4
SuperNacho <vanbellum@gmail.com>
2018-03-12 13:54:10 +0300

    added recyclerview code in fragment

--- app/src/main/java/geekgram/supernacho/ru/MainFragment.java
+++ app/src/main/java/geekgram/supernacho/ru/MainFragment.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import geekgram.supernacho.ru.adapter.RecyclerViewAdapter;
+import geekgram.supernacho.ru.model.PhotoModel;
 
 
 public class MainFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    RecyclerView recyclerView;
+    List<PhotoModel> photos;
 
     public MainFragment() {
     }
@@ -33,11 +44,33 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        initPhotosArray();
         initUI(view);
         return view;
     }
 
+    private void initPhotosArray() {
+        photos = new ArrayList<>();
+        boolean isFav;
+        for (int i = 0; i < 200; i++){
+            if (i%3 == 0){
+                isFav = true;
+            } else {
+                isFav = false;
+            }
+            photos.add(new PhotoModel(isFav, null));
+        }
+    }
+
     private void initUI(View view) {
+        recyclerView = view.findViewById(R.id.main_fragment_recycler_view);
+        if (recyclerView != null){
+            GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+            layoutManager.setOrientation(GridLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
+        }
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(photos);
+        recyclerView.setAdapter(adapter);
         FloatingActionButton fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +81,6 @@ public class MainFragment extends Fragment {
         });
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -72,19 +104,8 @@ public class MainFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
