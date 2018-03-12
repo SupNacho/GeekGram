a83f49b78aa1e2f35cf20a8fa7a09c32ca75c2e7
SuperNacho <vanbellum@gmail.com>
2018-03-12 13:51:23 +0300

    add new files

---
+++ app/src/main/java/geekgram/supernacho/ru/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,10 @@
+package geekgram.supernacho.ru.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by SuperNacho on 12.03.2018.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/geekgram/supernacho/ru/model/PhotoModel.java
@@ -0,0 +1,8 @@
+package geekgram.supernacho.ru.model;
+
+/**
+ * Created by SuperNacho on 12.03.2018.
+ */
+
+public class PhotoModel {
+}
