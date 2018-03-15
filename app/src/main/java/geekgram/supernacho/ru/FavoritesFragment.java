4d0042c0af2c12e1c4667ed04fc4e142a5a6850a
SuperNacho <vanbellum@gmail.com>
2018-03-15 18:53:31 +0300

    inflated with code to view favorite photos

--- app/src/main/java/geekgram/supernacho/ru/FavoritesFragment.java
+++ app/src/main/java/geekgram/supernacho/ru/FavoritesFragment.java
@@ -1,11 +1,28 @@
 package geekgram.supernacho.ru;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+
+import geekgram.supernacho.ru.adapter.PhotoInterface;
+import geekgram.supernacho.ru.adapter.RecyclerViewAdapter;
+import geekgram.supernacho.ru.model.PhotoModel;
+
+import static geekgram.supernacho.ru.AllPhotoFragment.IMG_URI;
+import static geekgram.supernacho.ru.AllPhotoFragment.IS_FAVORITE;
 
 
 /**
@@ -13,7 +30,7 @@ import android.view.ViewGroup;
  * Use the {@link FavoritesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FavoritesFragment extends Fragment {
+public class FavoritesFragment extends Fragment implements PhotoInterface{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -22,6 +39,11 @@ public class FavoritesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    private List<PhotoModel> photos;
+    private List<PhotoModel> favPhotos;
+    private RecyclerViewAdapter adapter;
 
 
     public FavoritesFragment() {
@@ -58,8 +80,67 @@ public class FavoritesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+        initPhotosArray();
+        initUI(view);
+        return view;
+    }
+    private void initPhotosArray() {
+        photos = ((MainActivity)getContext()).getPhotos();
+        favPhotos = ((MainActivity)getContext()).getFavPhotos();
+    }
+
+    private void initUI(View view) {
+        recyclerView = view.findViewById(R.id.favorites_fragment_recycler_view);
+        if (recyclerView != null){
+            GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+            layoutManager.setOrientation(GridLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
+        }
+        adapter = new RecyclerViewAdapter(((MainActivity)getContext()).getFavPhotos(), new WeakReference<PhotoInterface>(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void viewPhoto(int pos){
+        if (photos.get(pos).getPhotoSrc() != null) {
+            String imgUri = photos.get(pos).getPhotoSrc().toString();
+            Intent viewIntent = new Intent(getActivity(), FullscreenPhotoActivity.class);
+            viewIntent.putExtra(IMG_URI, imgUri);
+            viewIntent.putExtra(IS_FAVORITE, photos.get(pos).isFavorite());
+            startActivity(viewIntent);
+        } else {
+            Snackbar.make(getParentFragment().getView().findViewById(R.id.fab), "No such photo", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    public void addPhoto(Uri photoUri){
+        photos.add(new PhotoModel(false, photoUri));
+        Snackbar.make(getParentFragment().getView().findViewById(R.id.fab), "Photo added successfully", Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void deletePhoto(final int pos){
+        final PhotoModel[] tempPhoto = new PhotoModel[1];
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert
+                .setMessage(R.string.alert_delete_msg)
+                .setCancelable(true)
+                .setNegativeButton(R.string.alert_negative_txt, null)
+                .setPositiveButton(R.string.alert_positive_txt, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tempPhoto[0] = photos.remove(pos);
+                        adapter.notifyDataSetChanged();
+                        Snackbar.make(getParentFragment().getView().findViewById(R.id.fab), "Photo deleted", Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                photos.add(pos, tempPhoto[0]);
+                                adapter.notifyDataSetChanged();
+                            }
+                        }).show();
+                    }
+                })
+                .show();
+
     }
 
 }
