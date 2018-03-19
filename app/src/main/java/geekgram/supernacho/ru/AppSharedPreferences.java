befc1fb5295cc09bb7548a2d8065e26211aaf5d9
SuperNacho <vanbellum@gmail.com>
2018-03-19 17:53:35 +0300

    added shared preferences to project witch save chosen theme

--- app/src/main/java/geekgram/supernacho/ru/AppSharedPreferences.java
+++ app/src/main/java/geekgram/supernacho/ru/AppSharedPreferences.java
@@ -1,8 +1,29 @@
 package geekgram.supernacho.ru;
 
-/**
- * Created by SuperNacho on 19.03.2018.
- */
+
+import android.content.Context;
+import android.content.SharedPreferences;
 
 public class AppSharedPreferences {
+
+    private static final String APP_PREFS = "geek_gram_prefs";
+    private static final String APP_PREFS_THEME = "geek_gram_theme";
+
+    private Context context;
+    private SharedPreferences prefs;
+
+    public AppSharedPreferences(Context context) {
+        this.context = context;
+        prefs = context.getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
+    }
+
+    public void saveThemeId(int idTheme){
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(APP_PREFS_THEME, idTheme);
+        editor.apply();
+    }
+
+    public int getSavedTheme(){
+        return prefs.getInt(APP_PREFS_THEME, R.style.ThemeStandard);
+    }
 }
